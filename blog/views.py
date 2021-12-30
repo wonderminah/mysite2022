0f6b8ab625a23eb077a72276a32908ece837e8a6
Kim, Minah | Minah | MSD <minah.kim@rakuten.com>
2021-12-31 03:04:16 +0900

    Read markdown and Show as html - prototype

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+import markdown
 
 
 def index(request):
-    return render(request, 'blog/index.html')
+
+    file_path = '/Users/minah.kim/Desktop/MinahKimTechBlog/'
+    file_name = '1.md'
+    target_file = file_path + file_name
+
+    with open(target_file, 'r', encoding='UTF-8') as file:
+        text = file.read()
+
+    extensions = ["extra", "abbr", "attr_list", "def_list", "fenced_code", "footnotes", "md_in_html", "tables", "admonition", "codehilite", "legacy_attrs", "legacy_em", "meta", "nl2br", "sane_lists", "smarty", "toc", "wikilinks"]
+    html = markdown.markdown(text, extensions=extensions)
+    print(html)
+    body = '{}'.format(html)
+    context = {'title': 'this is title', 'body': body}
+
+    return render(request, 'blog/index.html', context)
