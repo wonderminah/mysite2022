3e806017b7dafbea7d689977e8754cfe2e597806
Kim, Minah | Minah | MSD <minah.kim@rakuten.com>
2022-01-04 02:36:05 +0900

    Add static directory

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ import markdown
 import os
 import time
 
-path = '/Users/minah.kim/Desktop/MinahKimTechBlog/'
+path = 'static/post/md'
 extensions = ["extra", "abbr", "attr_list", "def_list", "fenced_code", "footnotes", "md_in_html", "tables",
               "admonition", "codehilite", "legacy_attrs", "legacy_em", "meta", "nl2br", "sane_lists", "smarty",
               "toc", "wikilinks"]
